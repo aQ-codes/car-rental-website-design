7b092fdf8a8e26977bfe9a651bb4c07f1a5d671c
aq-codes <vpaqil3@gmail.com>
2024-08-21 14:31:25 +0530

    modified for mobile responsiveness

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
+//for toggling the menu bar 
+
 document.querySelector('#menu-icon').addEventListener('click', function() {
     document.querySelector('.social-nav').classList.toggle('active');
 });
 
 document.querySelector('#close-btn').addEventListener('click', function() {
     document.querySelector('.social-nav').classList.remove('active');
+});
+
+// for toggling the faq question and answers
+
+document.querySelector('.plus').addEventListener('click', function() {
+    document.querySelector('.faq-answer').classList.toggle('active');
+    document.querySelector('.close').classList.toggle('active');
+    document.querySelector('.plus').classList.toggle('inactive');
+});
+
+document.querySelector('.close').addEventListener('click', function() {
+    document.querySelector('.faq-answer').classList.remove('active');
+    document.querySelector('.close').classList.remove('active');
+    document.querySelector('.plus').classList.remove('inactive');
+
 });
 
